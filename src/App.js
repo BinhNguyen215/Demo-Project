394fee833c08b1cafe128f3524bfca713af2b5c4
BinhNguyen215 <127644891+BinhNguyen215@users.noreply.github.com>
2023-10-09 22:02:04 +0700

    React-Query

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Home from "./Pages/Home/Index";
 import { Provider } from "react-redux";
 import store from "./Redux/Store";
 import Events from "./Pages/Events/Index";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 function App() {
   const location = useLocation();
+  const queryClient = new QueryClient();
   return (
-    <Provider store={store}>
-      <div className="App">
-        <>
-          <Routes key={location.pathname} location={location}>
-            <Route path="/" element={<Home />} />
-            <Route path="events" element={<Events />} />
-          </Routes>
-        </>
-      </div>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <div className="App">
+          <>
+            <Routes key={location.pathname} location={location}>
+              <Route path="/" element={<Home />} />
+              <Route path="events" element={<Events />} />
+            </Routes>
+          </>
+        </div>
+      </Provider>
+    </QueryClientProvider>
   );
 }
 export default App;
--- src/Pages/Home/Index.js
+++ src/Pages/Home/Index.js
@@ -1,18 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./Home.css";
+import axios from "axios";
+import { useMutation } from "react-query";
 
+const fetchingPosts = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return res.data;
+};
 const Home = () => {
   const infors = useSelector((state) => state.infors);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const mutation = useMutation((payload) => {
+    axios.post("https://jsonplaceholder.typicode.com/posts", payload);
+  });
+
+  if (mutation.isLoading) return <div>loading...</div>;
+
+  if (mutation.isError) return <div>{mutation.error.message}</div>;
+
+  if (mutation.isSuccess) return window.alert("Thành công!!");
+
 
   return (
     <div>
       <div className="card">
-        <h2 className="card-title">Events Page</h2>  
+        <h2 className="card-title">Home Page</h2>  
         <div className="card-content">  This is Home Page</div>
         <Link to="Events">
-          <button className="card-button">Link to Home</button>
+          <button className="card-button">Link to Events</button>
         </Link>
       </div>
       <div className="event-list">
@@ -32,6 +51,33 @@ const Home = () => {
             </div>
           </div>
         ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          placeholder="Enter title"
+        />
+
+        <input
+          type="text"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+          placeholder="Enter body"
+        />
+
+        <button
+          onClick={() => {
+            mutation.mutate({ title, body });
+          }}
+        >
+          submit
+        </button>
       </div>
     </div>
   );
